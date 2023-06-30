761a98304dc07a918035ea8766fda5054a473fb1
Augusto Manzano <81631170+J-AugustoManzano@users.noreply.github.com>
2023-06-30 14:55:01 -0300

    Add files via upload

--- Cap 05/Topico 5.3/Topico 5.3.2/Pre-Teste Fluxo Falso/Interativo/atesejaINT.swift
+++ Cap 05/Topico 5.3/Topico 5.3.2/Pre-Teste Fluxo Falso/Interativo/atesejaINT.swift
@@ -1,16 +1,3 @@
-/*
- * Livro .....: Algoritmos
- *              Logica para Desenvolvimento de Programacao de
- *              Computadores
- * Autores ...: Jose Augusto N. G. Manzano
- *              Jayr Figueiredo de Oliveira
- * Editora ...: Erica
- *
- * Capitulo ..: 5 - Topico 5.3.2
- * 
- * Programa ..: LACO CONDICIONAL PRE-TESTE COM FLUXO FALSO INTERATIVO
- */
-
 import Foundation
 
 var N: Int = 0
@@ -19,16 +6,17 @@ var RESP: Character = "S"
 
 print("LACO PRE-TESTE COM FLUXO VERDADEIRO")
 print("Multiplicacao de numero por 3 por N vezes\n")
-
 RESP = "S"
-while (!(RESP != "S")) {
+while RESP == "S" {
     print("Entre valor numerico inteiro: ", terminator: "")
-    let input = readLine()
-    N = Int(input ?? "") ?? 0
-    R = N * 3
-    print("O numero informado X 3 = \(R)\n")
+    if let input = readLine(), let numericInput = Int(input) {
+        N = numericInput
+        R = N * 3
+        print("O numero informado X 3 = \(R)\n")
+    }
     print("Deseja continuar? (S/N): ", terminator: "")
-    let response = readLine()?.uppercased()
-    RESP = response?.first ?? "N"
+    if let response = readLine()?.uppercased() {
+        RESP = response.first ?? "N"
+    }
     print()
 }
--- Cap 05/Topico 5.3/Topico 5.3.2/Pre-Teste Fluxo Falso/Iterativo/atesejaIT.swift
+++ Cap 05/Topico 5.3/Topico 5.3.2/Pre-Teste Fluxo Falso/Iterativo/atesejaIT.swift
@@ -1,16 +1,3 @@
-/*
- * Livro .....: Algoritmos
- *              Logica para Desenvolvimento de Programacao de
- *              Computadores
- * Autores ...: Jose Augusto N. G. Manzano
- *              Jayr Figueiredo de Oliveira
- * Editora ...: Erica
- *
- * Capitulo ..: 5 - Topico 5.3.2
- * 
- * Programa ..: LACO CONDICIONAL PRE-TESTE COM FLUXO FALSO ITERATIVO
- */
-
 import Foundation
 
 var I: Int = 0
@@ -19,15 +6,14 @@ var R: Int = 0
 
 print("LACO PRE-TESTE COM FLUXO VERDADEIRO")
 print("Multiplicacao de numero por 3 por 5 vezes\n")
-
 I = 1
-while (!(I > 5)) {
+while I <= 5 {
     print("Calculo: \(I)")
     print("Entre valor numerico inteiro: ", terminator: "")
-    let input = readLine()
-    N = Int(input ?? "") ?? 0
-    R = N * 3
-    print("O numero informado X 3 = \(R)\n")
+    if let input = readLine(), let numericInput = Int(input) {
+        N = numericInput
+        R = N * 3
+        print("O numero informado X 3 = \(R)\n")
+    }
     I = I + 1
 }
-
