33dc1396d4617e79288343d6d2b754c7573e197b
Augusto Manzano <81631170+J-AugustoManzano@users.noreply.github.com>
2023-07-03 01:07:38 -0300

    Add files via upload

---
+++ Cap 05/Topico 5.5/Seletivo/Simulado/Com Enquanto-infinito/enqinfit.kt
@@ -0,0 +1,36 @@
+/*
+ * Livro .....: Algoritmos
+ *              Logica para Desenvolvimento de Programacao de
+ *              Computadores
+ * Autores ...: Jose Augusto N. G. Manzano
+ *              Jayr Figueiredo de Oliveira
+ * Editora ...: Erica
+ *
+ * Capitulo ..: 5 - Topico 5.5
+ * 
+ * Programa ..: LACO SELETIVO ITERATIVO (SIMULADO ENQUANTO)
+ * 
+ */
+
+import java.util.*
+
+fun main() {
+  
+    var I = 1
+    var N: Int
+    var R: Int
+    var entrada = Scanner(System.`in`)
+    
+    println("LACO PRE-TESTE COM FLUXO VERDADEIRO")
+    println("Multiplicacao de numero por 3 por 5 vezes\n")
+    while (true) {
+        println("Calculo: $I")
+        print("Entre varor numerico inteiro: ")
+        N = entrada.nextInt()
+        R = N * 3
+        println("O numero informado X 3 = $R\n")
+        if (I > 4) break
+        I = I + 1
+    }
+	
+}
