9b2342f63b9235dacdfc14860bc1fbc20f1252fd
Augusto Manzano <81631170+J-AugustoManzano@users.noreply.github.com>
2023-11-07 19:41:35 -0300

    Update polimuninc.cpp

--- Cap 12/12.7 - Poliformismo/12.7.1 - Poliformismo Universal de Inclusao/polimuninc.cpp
+++ Cap 12/12.7 - Poliformismo/12.7.1 - Poliformismo Universal de Inclusao/polimuninc.cpp
@@ -15,49 +15,40 @@
 #include <iostream>
 using namespace std;
 
-class CLS_PAI {
-public:
-  virtual ~CLS_PAI() {} // Destrutor virtual
-  virtual void EXECUTA() {
-    cout << "Acao executada na classe-pai" << endl;
-  }
+class CLS_PAI
+{
+  public:
+    virtual void EXECUTA()
+    {
+      cout << "Acao executada na classe-pai" << endl;
+    }
 };
 
-class CLS_FILHO : public CLS_PAI {
-public:
-  void EXECUTA() override {
-    cout << "Acao executada na classe-filho" << endl;
-  }
+class CLS_FILHO : public CLS_PAI
+{
+  public:
+    void EXECUTA() override
+    {
+      cout << "Acao executada na classe-filho" << endl;
+    }
 };
 
-int main() {
-	
-  CLS_PAI *PAI = new CLS_PAI();
-  CLS_FILHO *FILHO = new CLS_FILHO();
+int main(void)
+{
+  CLS_PAI PAI, *ptr_pai = nullptr;
+  CLS_FILHO FILHO;
 
-  // PAI.EXECUTA()
-  PAI->EXECUTA();
+  PAI.EXECUTA();
   
-  // FILHO.EXECUTA()
-  FILHO->EXECUTA();
+  FILHO.EXECUTA();
 
   // PAI <- FILHO
-  delete PAI;
-  PAI = FILHO;
-  
-  //  PAI.EXECUTA()
-  PAI->EXECUTA();
+  ptr_pai = &FILHO;
+  ptr_pai->EXECUTA();
 
   // PAI <- ""
-  delete PAI;
-  PAI = new CLS_PAI();
-  
-  // PAI.EXECUTA()
-  PAI->EXECUTA();
-
-  // Limpeza de memoria
-  delete PAI;
-  delete FILHO;
+  ptr_pai = &PAI;
+  ptr_pai->EXECUTA();
 
   return 0;
 }
